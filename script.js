ad2a1524398eadddc420a0cfe294aaf073e7e57c
Ana-Marija M <ana-marija.mitrevska@students.finki.ukim.mk>
2024-04-07 17:35:04 +0200

    feat: add movies in carousel

--- script.js
+++ script.js
@@ -1,62 +1,100 @@
-// function getQuote(){
-//     $.ajax({
-//         url: 'https://quoteapi.pythonanywhere.com/random',
-//         type: "GET",
-//         success: function(data){
-//             console.log(data);
-//             $("#quote").text(data.quote);
-//         },
-//         error: function(xhr, status, error){
-//             console.error('Error fetching quote:', error);
-//         }
 
-//     });
-// }
-
-// const apiUrl = 'https://quoteapi.pythonanywhere.com/quotes/';
-
-//     fetch(apiUrl)
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.error('Error:', error));
-
-// $(document).ready(function() {
-//     getQuote();
-// });
-
-document.addEventListener("DOMContentLoaded", function() {
-
-var carouselWidth = $(".carousel-inner.movie")[0].scrollWidth;
-var cardWidth = $(".carousel-item.movie").width();
-var scrollPosition = 0;
-
-$(".carousel-control-next").on("click", function () {
-    if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-      scrollPosition += cardWidth;  //update scroll position
-      $(".carousel-inner.movie").animate({ scrollLeft: scrollPosition },600); //scroll left
-    }
+$(document).ready(function () {
+  $(".carousel-control-next").on("click", function () {
+      var carouselWidth = $(this).closest('.carousel').find(".carousel-inner")[0].scrollWidth;
+      var cardWidth = $(this).closest('.carousel').find(".carousel-item").width();
+      var scrollPosition = $(this).closest('.carousel').find(".carousel-inner").scrollLeft();
+      
+      var scrollThreshold = carouselWidth - cardWidth * 4;
+      
+      if (scrollPosition < scrollThreshold) {
+          scrollPosition += cardWidth;
+          $(this).closest('.carousel').find(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+      }
   });
 
   $(".carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $(".carousel-inner.movie").animate(
-        { scrollLeft: scrollPosition },
-        600
-      );
-    }
+      var cardWidth = $(this).closest('.carousel').find(".carousel-item").width();
+      var scrollPosition = $(this).closest('.carousel').find(".carousel-inner").scrollLeft();
+      
+      if (scrollPosition > 0) {
+          scrollPosition -= cardWidth;
+          $(this).closest('.carousel').find(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+      }
   });
 
-  var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
-    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
-      wrap: false,
+  var currentYear = new Date().getFullYear();
+
+  var genres = ["Sci-fi", "Action", "Comedy", "Romance", "Thriller"];
+  var apikey = "a6592a8d";
+  var moviesByGenre = {}
+  
+  // Function to fetch movies by genre
+  function getMoviesByGenre(genre) {
+      $.ajax({
+        url: "http://omdbapi.com/?s=" + genre + "&apikey=" + apikey + "&type=movie",
+          dataType: "json",
+          success: function (data) {
+              if (data.Response == "True") {
+                  var movies = data.Search;
+                  moviesByGenre[genre] = movies;
+
+
+                  console.log(data)
+                  console.log(moviesByGenre)
+
+                  displayMovies(genre, movies);
+              } else {
+                console.log("Movies fetched for genre " + genre + " in year " + year);
+              }
+          },
+          error: function (xhr, status, error) {
+              console.log("Error fetching movies for genre: " + genre);
+          }
+      });
+  }  
+
+  
+  // Function to display movies in carousel
+  function displayMovies(genre, movies) {
+      var container = $("#" + genre.toLowerCase() + "-section");
+      var carouselInner = container.find(".carousel-inner.movie");
+
+      carouselInner.empty();
+
+      movies.forEach(function (movie, index) {
+          if (movie.Poster !== "N/A") {
+              var movieHtml = `
+                  <div class="carousel-item movie">
+                      <div class="card bg-dark">
+                          <div class="img-wrapper">
+                              <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'placeholder.jpg'}" class="d-block w-100 movie-img" alt="${movie.Title}">
+                          </div>
+                          <div class="card-body text-center">
+                              <h5 class="card-title">${movie.Title}</h5>
+                          </div>
+                      </div>
+                  </div>
+              `;
+              carouselInner.append(movieHtml);
+          }
+      });
+
+      container.find('.carousel-inner.movie').parent('.carousel').each(function () {
+        var carousel = new bootstrap.Carousel(this, {
+            interval: false,
+            wrap: false,
+        });
     });
-  } else {
-    $(multipleCardCarousel).addClass("slide");
   }
 
+  // Function to fetch movies for all genres
+  function fetchAllMovies() {
+      genres.forEach(function (genre) {
+          getMoviesByGenre(genre);
+      });
+  }
+
+  // Call fetchAllMovies function on document ready
+  fetchAllMovies();
 });
